9a60e2096495d5cf3875dff4b9584f3ff4d9b6ed
Lewis Moten <lewismoten@gmail.com>
2015-03-21 18:21:41 -0400

    #1 remove football background

--- dropkick.js
+++ dropkick.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("movie");
 var context = canvas.getContext("2d");
 var baby = document.createElement("img");
+var football = document.createElement("img");
 
 var babyOutline = [
   115,49,128,40,150,36,170,38,191,50,201,76,195,101,199,120,196,139,194,144,219,
@@ -12,12 +13,21 @@ var babyOutline = [
   110,127,103,109,107,104,113,106,107,84,106,64
 ];
 
+var footballOutline = [
+  7,36,33,20,59,16,81,19,107,33,123,53,123,60,113,70,95,
+  83,79,88,53,84,27,74,11,60,3,45
+]
+
 baby.onload = function() {
-  var width = 512 * baby.width / baby.height;
+  var width = 341;
   var height = 512;
   drawClippedImage(context, baby, 0, 0, width, height, babyOutline);
+}
 
-  context.restore();
+football.onload = function() {
+  var width = 125;
+  var height = 100;
+  drawClippedImage(context, football, 0, 0, width, height, footballOutline);
 }
 
 function drawClippedImage(context, image, x, y, width, height, clipCoordinates) {
@@ -32,6 +42,8 @@ function drawClippedImage(context, image, x, y, width, height, clipCoordinates) 
   context.closePath();
   context.clip();
   context.drawImage(image, x, y, width, height);
+  context.restore();
 }
 
 baby.src = "http://upload.wikimedia.org/wikipedia/commons/2/26/Baby_in_an_infant_bodysuit.jpg";
+football.src = "http://upload.wikimedia.org/wikipedia/commons/6/64/Football_signed_by_Gerald_R._Ford.jpg";
