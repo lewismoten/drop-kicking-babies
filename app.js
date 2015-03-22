dd4b28f6c1b827f93e6f65d6c6c8295e0153dbcd
Lewis Moten <lewismoten@gmail.com>
2015-03-22 19:39:20 -0400

    #5 wire up drop-kick button to controller

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 (function(){
   var app = angular.module('dropkick', []);
-  app.controller('DropKickBabyController', function($http) {
-    var self = this;
-    self.kickers = [];
-    self.kickees = [];
-    $http.get('kickers.json').then(function(res) {self.kickers = res.data;});
-    $http.get('kickees.json').then(function(res) {self.kickees = res.data;});
-  });
+  app.controller('DropKickBabyController', [
+    '$scope', '$http',
+    function($scope, $http, dropkickBaby) {
+      var self = this;
+      self.kickers = [];
+      self.kickees = [];
+      $http.get('kickers.json').then(function(res) {self.kickers = res.data;});
+      $http.get('kickees.json').then(function(res) {self.kickees = res.data;});
+      $scope.dropkickBaby = function() {
+        balls.push(0);
+      };
+    }
+  ]); // DropKickBabyController
 })();
