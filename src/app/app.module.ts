7c25d57a341df8295dd8a90cb55213744dd6ddb5
Lewis Moten <lewismoten@gmail.com>
2023-07-29 21:05:40 -0400

    Revert "bump docs into root"
    This reverts commit fb5d388f72411b07121bf250e1ba8ce6aaf83686.

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { DropKickComponent } from './drop-kick/drop-kick.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: DropKickComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    DropKickComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
---
+++ src/app/drop-kick/Coordinate2DList.ts
@@ -0,0 +1,5 @@
+import Coordinate2D from './Coordinate2D';
+
+type Coordinate2DList = Array<Coordinate2D>;
+
+export default Coordinate2DList;
